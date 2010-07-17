64ec92c702bb02e828729d338b05468424aae3b8
Andrew Roth <andrewroth@gmail.com>
2010-07-17 01:12:58 -0400

    put most recent games on the top

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.xml
   def index
-    @games = Game.all
+    @games = Game.all :order => "played_at DESC"
 
     respond_to do |format|
       format.html # index.html.erb
